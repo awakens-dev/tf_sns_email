3b4137a2068e52ad4c930aa0cd50d2c0237838fb
enm10k <enm10k@gmail.com>
2019-06-02 21:20:41 +0900

    Upgrade to terraform v0.12

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
-output "arn" {
-  value       = "${aws_cloudformation_stack.sns-topic.outputs["ARN"]}"
+data "aws_sns_topic" "output" {
+  name = element(reverse(split(":", aws_cloudformation_stack.sns-topic.outputs["ARN"])), 0)
+}
+
+output "sns_topic" {
+  value       = data.aws_sns_topic.output
   description = "Email SNS topic ARN"
 }
